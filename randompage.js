6a9dc79984fbcbfe71e12690032f5cb6db35682c
ryomad110 <57401746+ryomad110@users.noreply.github.com>
2021-12-03 17:24:19 +0900

    Update randompage.js

--- randompage.js
+++ randompage.js
@@ -2,4 +2,4 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 var links = ['https://forms.gle/KsCTvLTuz9rtiL7b9 ', 'https://forms.gle/pEHhjH31nT8NFYsQ7', 'https://forms.gle/cmxRbv2A53QuCfoh6', 'https://forms.gle/VKiSu29BzabAZgZ19', 'https://forms.gle/DNzZf2EzyceLg9Lq7']; //同じようにして遷移先のURLを配列へ格納する
-window.location.href = links[getRandomInt(3)]; //getrandomInt(x)のxを遷移先のページの数に
+window.location.href = links[getRandomInt(5)]; //getrandomInt(x)のxを遷移先のページの数に
